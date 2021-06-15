68164f1e9efdf3c89f43183eb2777ee46aa5e431
brunocamps <bruno.mcamps@outlook.com>
2021-06-15 15:07:46 -0300

    Added a toast service for notifications

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { MemberListComponent } from './members/member-list/member-list.component
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './members/lists/lists.component';
 import { MessagesComponent } from './members/messages/messages.component';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,10 @@ import { MessagesComponent } from './members/messages/messages.component';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -13,7 +14,8 @@ export class NavComponent implements OnInit {
   model: any = {}
 
 
-  constructor(public accountService: AccountService, private router: Router) { }
+  constructor(public accountService: AccountService, 
+    private router: Router, private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -24,6 +26,7 @@ export class NavComponent implements OnInit {
       this.router.navigateByUrl('/members');
     }, error => {
       console.log(error);
+      this.toastr.error(error.error);
     })
   }
 
